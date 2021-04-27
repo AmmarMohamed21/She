6e3c39f6a9b887c7f7c5f6116592c622e39ef9cf
Ammar Mohamed Sobhi <ammarmohamed13@gmail.com>
2021-04-27 02:58:41 +0200

    Update women_safety_screen.dart

--- lib/servicesscreens/women_safety_screen.dart
+++ lib/servicesscreens/women_safety_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:she/constants.dart';
 import '../components/main_app_bar.dart';
 
 class WomenSafetyScreen extends StatelessWidget {
@@ -7,7 +9,44 @@ class WomenSafetyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getAppBar(title: 'Women Safety', context: context),
-      body: Text('Women Safety'),
+      body: DefaultTabController(
+        length: 3,
+        child: TabBar(
+          indicatorColor: kLightPrimaryColor,
+          labelColor: kPrimaryText,
+          tabs: [
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(FontAwesomeIcons.newspaper),
+                  Text('Blogs'),
+                ],
+              ),
+              // icon: Icon(FontAwesomeIcons.newspaper),
+              // text: 'Blogs',
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(FontAwesomeIcons.video),
+                  Text('Videos'),
+                ],
+              ),
+            ),
+            Tab(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(FontAwesomeIcons.ban),
+                  Text('Report'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
